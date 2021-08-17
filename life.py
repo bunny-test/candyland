3a1e80ac6281c4191fdf7ac57f45d206904dadc9
Stepa <kiresarmas@gmail.com>
2021-08-18 00:41:08 +0300

    Test GPG signature

--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-from cat import Cat
+import cat
 
-my_cat = Cat('mittens', strength=11, will=13)
+my_cat = cat.Cat('mittens', strength=11, will=13)
 my_cat.drop(keys=6, clock=17, glass=11, remote=2, tv=14)
